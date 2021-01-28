0880e393197d7e50b4ecf86e4cc8a2e8a4fc8cfb
glamboyosa <ogbemudiatimothy@gmail.com>
2021-01-28 01:41:05 +0100

    feat: redirect page exchanges code for token

--- pages/redirect.jsx
+++ pages/redirect.jsx
@@ -1,11 +1,42 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-const Redirect = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Redirect</title>
-    </Head>
-    <h1>Page for redirection</h1>
-  </div>
-);
+const Redirect = ({ client_id, client_secret }) => {
+  console.log(client_id);
+  console.log(client_secret);
+  useEffect(() => {
+    if (window.location.search.split('=')[1]) {
+      fetch('https://slack.com/api/oauth.access', {
+        body: new URLSearchParams({
+          client_id,
+          client_secret,
+          code: window.location.search.split('=')[1],
+        }),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+        .then((resp) => resp.json())
+        .then((resp) => console.log(resp))
+        .catch((err) => console.log(err.message));
+    }
+  }, []);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Redirect</title>
+      </Head>
+      <h1>Page for redirection</h1>
+    </div>
+  );
+};
+export function getStaticProps() {
+  return {
+    props: {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+    },
+  };
+}
 export default Redirect;
